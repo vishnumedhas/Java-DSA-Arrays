a9d951d26c745c356b1b13031a691da928f44098
vishnumedhas <vishnumedhas11@gmail.com>
2026-08-09 16:06:44 +0530

    4th program successful

---
+++ ReverseArray.java
@@ -0,0 +1,20 @@
+public class ReverseArray {
+    
+    public static  void reverseNumbers(int []numbers){
+        int start=0; int end=numbers.length-1;
+        while(start<end){
+            int temp=numbers[end];
+             numbers[end]=numbers[start];
+             numbers[start]=temp;
+             start ++;
+             end --;
+        }
+    }
+    public static void main(String[] args) {
+        int []numbers={6,12,38,2,17,34,51,47};
+        reverseNumbers(numbers);
+        for(int i=0;i<numbers.length;i++){
+       System.out.print(numbers[i]+" ");
+        }
+    }
+}
